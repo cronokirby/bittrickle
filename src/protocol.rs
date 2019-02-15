805b817cb84d20830a510408ceaa2080260ba989
Lúcás Meier <cronokirby@gmail.com>
2019-02-15 22:19:56 +0100

    Add basic Parse scaffolding

--- src/protocol.rs
+++ src/protocol.rs
@@ -1,6 +1,21 @@
+/// Represents different parse errors for the protocol
+pub enum ParseError {
+    /// This action was unkown
+    UnknownAction,
+    /// The byte size for these bytes wasn't correct
+    BadSize {
+        expected: usize,
+        got: usize
+    }
+}
+
+/// A specialized `Result` for ParseErrors
+pub type ParseResult<T> = Result<T, ParseError>;
+
+
 /// Used to communicate intent between the client and the tracker
 #[derive(Debug, Clone)]
-enum Action {
+pub enum Action {
     /// The client wishes to connect to the tracker
     Connect,
     /// The client wants announce information from the tracker
@@ -11,9 +26,22 @@ enum Action {
     Error
 }
 
+impl Action {
+    fn from_u32(u: u32) -> ParseResult<Self> {
+        match u {
+            0 => Ok(Action::Connect),
+            1 => Ok(Action::Announce),
+            2 => Ok(Action::Scrape),
+            3 => Ok(Action::Error),
+            _ => Err(ParseError::UnknownAction)
+        }
+    }
+
+}
+
 /// Represents an initial request from the client
 #[derive(Debug, Clone)]
-struct ConnectRequest {
+pub struct ConnectRequest {
     /// Always a magic 0x41727101980
     protocol_id: u64,
     /// For valid requests, always be `Action::Connect`
@@ -22,13 +50,23 @@ struct ConnectRequest {
     transaction_id: u32
 }
 
+impl ConnectRequest {
+    fn from_bytes(data: &[u8]) -> ParseResult<Self> {
+        let len = data.len();
+        if len != 16 {
+            return Err(ParseError::BadSize { expected: 16, got: len });
+        }
+        unimplemented!()
+    }
+}
+
 /// A random ID identifying a tracker connection
 #[derive(Debug, Copy, Clone)]
-struct ConnectionID(u64);
+pub struct ConnectionID(u64);
 
 /// Represents the tracker response for a `ConnectRequest`
 #[derive(Debug, Clone)]
-struct ConnectResponse {
+pub struct ConnectResponse {
     /// Should always be 0
     action: Action,
     /// The transaction ID identifying the client
