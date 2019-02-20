9a589509fec60d2bddfa203149dcf3fd1e16500a
Lúcás Meier <cronokirby@gmail.com>
2019-02-20 21:00:52 +0100

    Add basic upd request loop

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
 mod protocol;
+use protocol::Request;
+
+use std::net::UdpSocket;
 
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> std::io::Result<()> {
+    let mut socket = UdpSocket::bind("127.0.0.1:8080")?;
+    let mut buffer = vec![0; 2048];
+    loop {
+        let (amt, src) = socket.recv_from(&mut buffer)?;
+        let request = Request::from_bytes(&buffer[..amt]);
+        println!("New request from {:?}", src);
+        println!("{:?}", request);
+    }
+    Ok(())
 }
