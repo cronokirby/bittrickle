89eb720a99f38716cad661998330a78df1155b75
Lúcás Meier <cronokirby@gmail.com>
2019-02-23 22:35:07 +0100

    Add basic server struct

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,10 @@
 extern crate rand;
 
 mod protocol;
-use protocol::Request;
+mod server;
 
-use std::net::{SocketAddr, UdpSocket};
-
-
-fn handle_request(src: SocketAddr, request: &Request) {
-    println!("New request from {:?}", src);
-    println!("{:?}", request);
-}
 
 fn main() -> std::io::Result<()> {
-    let mut socket = UdpSocket::bind("127.0.0.1:8080")?;
-    let mut buffer = vec![0; 2048];
-    loop {
-        let (amt, src) = socket.recv_from(&mut buffer)?;
-        let request = Request::from_bytes(&buffer[..amt]);
-        request.map(|r| handle_request(src, &r));
-    }
-    Ok(())
+    let mut server = server::Server::new("127.0.0.1:8080")?;
+    server.run()
 }
